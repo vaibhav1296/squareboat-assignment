4938142a860bd3b1e7063363052ee6d51edb322f
vaibhav1296 <sharmavaibhav5796@gmail.com>
2022-02-12 03:05:52 +0530

    config file includes error codes

---
+++ config/config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    errorCodes:{
+        INVALID_PARAM:600,
+        EMPTY_FIELD:601,
+        PASSWORD:602,
+        DB_QUERY:603,
+        JWT:604,
+        NOT_IN_DB:605,
+        LOG_IN:606
+    }
+}
