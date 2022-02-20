414edfa36701a6766511851608c836816b76b581
vaibhav1296 <sharmavaibhav5796@gmail.com>
2022-02-20 13:18:03 +0530

    cors added

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 require('dotenv').config()
 const express = require('express')
 const app = express()
+const cors = require('cors')
 const mongoose = require('mongoose')
 const userController = require('./controllers/userController')
-
+app.use(cors())
 app.use(express.json());
 app.use(express.urlencoded({extended:true}))
 
